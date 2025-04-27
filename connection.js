6eec6fbdebd40b13cb4111c4095baa444326b9c4
Samarkun <samarkun4@gmail.com>
2025-04-27 20:04:06 +0530

    connecting database

---
+++ connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+async function connectToMongoDB(url) {
+   return mongoose.connect(url) 
+}
+
+module.exports = {
+    connectToMongoDB,
+}
+
+
+
+
+
+
+
+
+
+
