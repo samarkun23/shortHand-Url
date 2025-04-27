2f3d9781b2b494b273345aaf6b5c72657f393f64
Samarkun <samarkun4@gmail.com>
2025-04-27 20:04:54 +0530

    update the router and connect mongo db

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express")
-
+const { connectToMongoDB } = require('./connection')
+const urlRoute = require('./routes/url')
 const app = express();
 const PORT = 8001
+
+connectToMongoDB('mongodb://localhost:27017/short-url')
+.then(() => console.log("Mongodb connected"))
+
+app.use('/', urlRoute)
 
 app.listen(PORT, console.log(`Server started at PORT:${PORT}`))
 
