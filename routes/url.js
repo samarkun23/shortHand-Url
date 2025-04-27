e0bb43ac9ee2af329353b44758317595d48dd4eb
Samarkun <samarkun4@gmail.com>
2025-04-27 20:03:34 +0530

    router has been updated

---
+++ routes/url.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const {handleGenerateNewShortURL} = require('../controllers/url')
+const router = express.Router();
+
+
+router.post('/', handleGenerateNewShortURL);
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
