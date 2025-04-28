ca4d07857cb165aeba643207984180c6389ec471
Samarkun <samarkun4@gmail.com>
2025-04-28 08:07:57 +0530

    parsing middleware and generates a new short hand url and returns the shortened URL in the format ex.com/random-id

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const { connectToMongoDB } = require('./connection')
 const urlRoute = require('./routes/url')
 const app = express();
 const PORT = 8001
+const Url = require('./models/url')
 
 connectToMongoDB('mongodb://localhost:27017/short-url')
 .then(() => console.log("Mongodb connected"))
 
-app.use('/', urlRoute)
+//for parsing the body we use one middleware
+app.use(express.json()) 
+
+app.use('/url', urlRoute)
+app.get('/:shortId', async(req, res) => {
+    const shortId = req.params.shortId;
+    const entry  = await Url.findOneAndUpdate({
+        shortId
+    }, {$push:{
+        visitHistory: {
+            timestamp: Date.now()
+        }
+    }})
+    res.redirect(entry.redirectURL)
+})
+
+
 
 app.listen(PORT, console.log(`Server started at PORT:${PORT}`))
 
