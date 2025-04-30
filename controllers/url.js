d3a9f55b7d2ecb4635e43892d8c8623ce8879567
Samarkun <samarkun4@gmail.com>
2025-04-30 16:15:53 +0530

    render the home page with short id

--- controllers/url.js
+++ controllers/url.js
@@ -11,8 +11,12 @@ async function handleGenerateNewShortURL(req, res) {
         redirectURL: body.url,
         visitHistory: [],
     })
-
-    return res.json({ id: shortId })
+    
+    return res.render('home', {
+        id: shortId
+    })
+    //here we send the json response but we want render a ui so
+    // return res.json({ id: shortId })
 }
 
 async function handleGetAnalytics(req, res) {
