21180758e258c61e0df6427cbf5f31c85d77e0b2
Samarkun <samarkun4@gmail.com>
2025-05-01 15:30:11 +0530

    fix bug - reading the mistake in cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const { getUser } = require('../uuidService/auth')
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-    const userUid = req.cookies.uid;
+    const userUid = req.cookie.uid;
     if (!userUid) return res.redirect('/login'); 
     
     const user = getUser(userUid)
