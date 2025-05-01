93edda84ba8988de9e319ec89728fbc728f9004b
Samarkun <samarkun4@gmail.com>
2025-05-01 15:32:00 +0530

    fix bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const { getUser } = require('../uuidService/auth')
 
 async function restrictToLoggedinUserOnly(req, res, next) {
-    const userUid = req.cookie?.uid;
+    const userUid = req.cookies?.uid;
     if (!userUid) return res.redirect('/login'); 
     
     const user = getUser(userUid)
