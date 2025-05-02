d9fe2450f077a6b59ab7408640648831d0ac1a90
Samarkun <samarkun4@gmail.com>
2025-05-02 22:03:09 +0530

    calling setuser fun using jwt

--- controllers/user.js
+++ controllers/user.js
@@ -21,10 +21,9 @@ async function handleUserLogin(req, res) {
         error: "Invalid Email and Password"
     })
     
-    //for uuid
-    const sessionId = uuidv4();    
-    setUser(sessionId, user);
-    res.cookie('uid', sessionId)
+        
+    const token = setUser(user);
+    res.cookie('uid', token)
     return res.redirect("/"); 
 }
 
