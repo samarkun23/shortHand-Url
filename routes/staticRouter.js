fed58bdfcdf1d551dc8dbe529fdf26ad5a4449c9
Samarkun <samarkun4@gmail.com>
2025-05-06 10:23:40 +0530

    adding a admin for access

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,12 +3,21 @@ const URL = require('../models/url');
 const { restricTo } = require('../middleware/auth');
 const router = express.Router();
 
-router.get('/', restricTo(['NORMAL']) ,async(req,res)=> {
+router.get('/', restricTo(['NORMAL','ADMIN']) ,async(req,res)=> {
     const allUrls = await URL.find({ createdBy: req.user._id })
     return res.render('home', {
         urls: allUrls
     })
 })
+
+//adding a router for admin
+router.get('/admin/urls', restricTo(['ADMIN']) , async (req, res) => {
+   const allUrls = await URL.find({createdBy: req.user._id})
+   return res.render('home', {
+        urls:allUrls
+   }) 
+})
+
 
 router.get('/signup', (req, res) => {
     return res.render('signup')
